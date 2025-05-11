80fea82d5db50ba735360932294427d4db5efd9e
Muhammad Rafli <154863009+raaflii@users.noreply.github.com>
2025-05-11 16:51:41 +0700

    remove all session variable, cookies, and destroy session

--- logout.php
+++ logout.php
@@ -3,5 +3,11 @@ session_start();
 
 // Hapus semua session variables, destroy the session, lalu redirect ke halaman login
 
+setcookie(session_name(), "", time() - 3600);
+
+session_unset();
+session_destroy();
+header("Location: login.php");
+
+
 exit;
-?> 
