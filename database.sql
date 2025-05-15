d21f7d57cdceeccbda7e713da2100fae5f386fd6
raaflii <rafliwrzky@gmail.com>
2025-05-15 08:58:53 +0700

    change comment and create table

--- database.sql
+++ database.sql
@@ -5,10 +5,12 @@ CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(50) NOT NULL,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  role ENUM('admin', 'user') DEFAULT 'user',
+  role varchar(50) DEFAULT 'user',
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- add admin user
+-- pastiin paswordnya di hash make generate_hash.php
 INSERT INTO users (username, email, password, role)
 VALUES ('admin', 'admin@example.com', '12345', 'admin');
 
