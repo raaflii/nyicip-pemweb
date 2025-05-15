0eb5f1128bf9a6f1d31111fa6fcacc8ae549221a
raaflii <rafliwrzky@gmail.com>
2025-05-15 09:00:27 +0700

    update algo to bcrypt

--- includes/register_process.php
+++ includes/register_process.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
     $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
     $stmt->bind_param("sss", $username, $email, $hashed_password);
